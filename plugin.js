f70e3e94cfcd7bd6b8f40920aed9d636037ed4a0
Sam Parsons <samparsons269@gmail.com>
2021-05-24 20:41:26 -0400

    first draft

--- plugin.js
+++ plugin.js
@@ -1,5 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const coffee = require('coffeescript');
+
 module.exports = function plugin(config, options) {
   return {
     name: 'snowpack-plugin-coffeescript',
+    resolve: {
+      input: ['.coffee'],
+      output: ['.js'],
+    },
+    load({ filePath, fileExt }) {
+      const code = fs.readFileSync(filePath);
+      const options = {
+        sourceMap: true,
+        bare: true,
+        extensions: [fileExt],
+      };
+
+      const output = coffee.compile(code.toString(), options);
+
+      return output;
+    },
   };
 };
