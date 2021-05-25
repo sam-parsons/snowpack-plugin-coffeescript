f261cd391f8741e76d32741ec3f4e50f0f6a43cf
Sam Parsons <samparsons269@gmail.com>
2021-05-24 20:40:51 -0400

    basic compile case and added fixtures

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,8 +1,22 @@
 const plugin = require('../plugin');
+const path = require('path');
+const fs = require('fs');
 
 describe('snowpack run script plugin', () => {
   test('invokes plugin and returns correctly shaped object without error', () => {
     const obj = plugin();
     expect(obj.name).toEqual('snowpack-plugin-coffeescript');
+  });
+
+  test('transforms coffeescript code into plain javascript', () => {
+    const obj = plugin();
+    const before = obj.load({
+      filePath: path.join(__dirname, './fixtures/basic/before.coffee'),
+      fileExt: '.coffee',
+    }).js;
+    const after = fs
+      .readFileSync(path.join(__dirname, './fixtures/basic/after.js'))
+      .toString();
+    expect(before).toEqual(after);
   });
 });
